bb5de1c584465f615a7c527b7a0ecad173d23782
Guenton Brooks <guenton.brooks@ibis-management.com>
2022-10-13 12:48:56 -0400

    added mock login form

--- src/components/containers/Login.svelte
+++ src/components/containers/Login.svelte
@@ -1,9 +1,9 @@
 <script>
-  import LoginOpacity from "./LoginOpacity.svelte"
+  import LoginImage from "./LoginImage.svelte"
   import LoginContent from "./LoginContent.svelte"
 </script>
 
-<div class="flex container h-full">
-  <LoginOpacity />
+<div class="flex flex-col-reverse md:flex-row container h-full">
+  <LoginImage />
   <LoginContent />
 </div>
--- src/components/containers/LoginImage.svelte
+++ src/components/containers/LoginImage.svelte
@@ -7,11 +7,11 @@
 
 <style>
   .scoped-container {
-    background-image: url("img/apple-weight.jpg")
+    background-image: url("img/apple-weight.jpg");
   }
 </style>
 
 <div class="flex-1 flex justify-evenly flex-col h-full bg-fixed bg-cover bg-center bg-no-repeat scoped-container">
-  <h1 class="flex-1 text-center">{quote.text}</h1>
-  <h1 class="flex-1 text-center">{quote.author}</h1>
+  <h1 class="text-center font-serif italic text-xl">"{quote.text}"</h1>
+  <h1 class="text-center font-bold text-2xl">{quote.author}</h1>
 </div>
