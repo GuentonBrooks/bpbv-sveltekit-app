6032f1d37f25831861039d157a9e42f5f9b5cbb1
Guenton Brooks <guenton.brooks@ibis-management.com>
2022-10-31 13:46:11 -0400

    fixed table fitment

--- src/components/forms/BpbvTable.svelte
+++ src/components/forms/BpbvTable.svelte
@@ -3,7 +3,7 @@
   import BpbvTableHead from "../tables/BpbvTableHead.svelte";
 </script>
 
-<section class="antialiased bg-gray-100 text-gray-600 h-screen px-4">
+<section class="antialiased bg-gray-100 text-gray-600 h-fit px-4 pt-20 pb-4">
   <div class="flex flex-col justify-center h-full">
     <div class="w-full mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
         
